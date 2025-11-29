cc8f6c7bd4f416d1d149afb28327db5435d369d5
norma <132732115+norma2627@users.noreply.github.com>
2025-11-29 19:10:52 +0900

    Finish: Compleated Chapter 1 of The Rust Programming Language

---
+++ TRPL/projects/hello_cargo/target/debug/deps/hello_cargo-aaa74157775a44fe.d
@@ -0,0 +1,5 @@
+/home/hikaru/Rust_Tutorial/TRPL/projects/hello_cargo/target/debug/deps/hello_cargo-aaa74157775a44fe.d: src/main.rs
+
+/home/hikaru/Rust_Tutorial/TRPL/projects/hello_cargo/target/debug/deps/hello_cargo-aaa74157775a44fe: src/main.rs
+
+src/main.rs:
---
+++ TRPL/projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/hikaru/Rust_Tutorial/TRPL/projects/hello_cargo/target/debug/hello_cargo: /home/hikaru/Rust_Tutorial/TRPL/projects/hello_cargo/src/main.rs
